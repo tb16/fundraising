f844db3ee2b3a34d362deba4ba2cbf7a5e8feb7c
Tek Prasad Basel <Komal@Teks-MacBook-Pro.local>
2016-10-12 19:04:43 -0700

    model done

--- modeling/model.py
+++ modeling/model.py
@@ -1,104 +1,37 @@
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.cross_validation import train_test_split
-from sklearn.cross_validation import cross_val_score
-from sklearn.naive_bayes import MultinomialNB as mnb
-from nltk.tokenize import word_tokenize, RegexpTokenizer
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
-import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.grid_search import GridSearchCV
+from roc_confusion import plot_roc, plot_confusion_matrix
+import matplotlib.pyplot as plt
 import cPickle as pickle
+import numpy as np
+
+with open('../data/vectorizer.pkl') as f:
+    vectorizer = pickle.load(f)
+with open('../data/sparse_mat.pkl') as f:
+    sparse_mat = pickle.load(f)
+with open('../data/randomforest.pkl') as f:
+    rf = pickle.load(f)
+with open('../data/gradboost.pkl') as f:
+    gbc = pickle.load(f)
 
 
+def plot_importance(clf, max_features=10):
+    '''Plot feature importance'''
+    
+    feature_importance = clf.feature_importances_
+    # make importances relative to max importance
+    feature_importance = 100.0 * (feature_importance / feature_importance.max())
+    sorted_idx = np.argsort(feature_importance)
+    pos = np.arange(sorted_idx.shape[0]) + .5
 
-def rf_model(X_train, X_test, y_train, y_test):
-    rf = RandomForestClassifier(max_depth= 4,max_features= 57,min_samples_leaf= 1,\
- n_estimators= 100, n_jobs = -1).fit(X_train, y_train)
-    recall = cross_val_score(rf, X_train, y_train, scoring = 'recall', cv = 5).mean()
-    precision = cross_val_score(rf, X_train, y_train, scoring = 'precision', cv = 5).mean()
-    accuracy = cross_val_score(rf, X_train, y_train, scoring = 'accuracy', cv = 5).mean()
-    print 'validation set'
-    print 'recall: ',recall
-    print 'precision: ', precision
-    print 'accuracy: ', accuracy
-    y_pred = rf.predict(X_test)
-    print
+    # Show only top features
+    pos = pos[-max_features:]
+    feature_importance = (feature_importance[sorted_idx])[-max_features:]
+    feature_names = (.columns[sorted_idx])[-max_features:]
 
-    accuracy  = accuracy_score(y_test,y_pred)
-    precision = precision_score(y_test,y_pred)
-    recall =    recall_score(y_test, y_pred)
-    importances = rf.feature_importances_
-    important_indices = np.argsort(rf.feature_importances_)[-1::-1]
-    print
-#     importance_summary = dict()
-#     for feature, importance in izip(df_X.columns[important_indices], importances[important_indices]):
-#          importance_summary[feature] = importance
-    return rf, accuracy, precision, recall
+    plt.barh(pos, feature_importance, align='center')
+    plt.yticks(pos, feature_names)
+    plt.xlabel('Relative Importance')
+    plt.title('Variable Importance')
 
-
-def search_best_params(X, y):
-    '''
-
-
-    '''
-    rf = RandomForestClassifier(n_jobs = -1)
-    param_grid = {
-              'n_estimators' : [100, 200, 400],
-              'max_depth' : [1, 2, 4],
-              'min_samples_leaf': [1, 2, 4],
-              'max_features': [10, 30, 57]
-             }
-    gs_cv = GridSearchCV(rf, param_grid, scoring = 'accuracy').fit(X,y)
-    return gs_cv.best_params_, gs_cv.best_estimator_
-
-
-
-def feature_mat():
-    '''
-    returns the feature matrix and labels
-    '''
-    with open('../data/vectorizer.pkl') as f:
-        vectorizer = pickle.load(f)
-    with open('../data/sparse_mat.pkl') as f:
-        sparse_mat = pickle.load(f)
-
-    with open('../data/nmf_model.pkl') as f:
-        nmf_model = pickle.load(f)
-    with open('../data/mnb_model.pkl') as f:
-        mnb_model = pickle.load(f)
-    df = pd.read_csv('../data/featured_data.csv')
-    df2 = pd.get_dummies(df.category)
-    df = pd.concat((df, df2), axis =1)
-
-    nmf_topics = nmf_model.transform(sparse_mat.toarray())
-    df3 = pd.DataFrame(nmf_topics)
-    df = pd.concat((df, df3), axis = 1)
-    df['mnb_probs'] = mnb_model.predict_proba(sparse_mat)[:,1]
-
-    y = (df.percentage > 0.5).astype(int)
-    drop_list = ['category', 'name','place','story','title','date_created','date_recorded','raised','people','percentage',\
-                'average_contribution']
-
-    df.drop(drop_list, axis =1, inplace = True)
-    X = df.values
-
-    return X, y
-
-
-def pickle_model(model_name, X, y):
-    '''
-
-    '''
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
-    X_train1, X_train2,y_train1, y_train2 = train_test_split(X_train, y_train, test_size = 0.8, random_state = 1234)
-    with open('../data/{}_model.pkl'.format(model_name), 'w') as f:
-        pickle.dump(rf, f)
 
 if __name__ == '__main__':
-    X, y = feature_mat()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
-    X_train1, X_train2,y_train1, y_train2 = train_test_split(X_train, y_train, test_size = 0.8, random_state = 1234)
-    rf, accuracy, precision, recall = rf_model(X_train2, X_test, y_train2, y_test)
-    print accuracy, precision, recall
+    plot_importance(rf)
--- modeling/model_comparison.py
+++ modeling/model_comparison.py
@@ -1,111 +1,57 @@
-import numpy as np
 import pandas as pd
-from itertools import izip
-import matplotlib.pyplot as plt
-from sklearn.cross_validation import StratifiedKFold
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, \
+GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, roc_auc_score
-from sklearn.cross_validation import KFold
-from sklearn.cross_validation import cross_val_score
-from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score,\
+ confusion_matrix, roc_auc_score, f1_score
 from sklearn.linear_model import LogisticRegression
-from roc_confusion import plot_roc, plot_confusion_matrix
-from model import feature_mat
+import cPickle as pickle
 
 
-def random_forest():
+def run_model(Model, X_train, X_test, y_train, y_test):
     '''
-
+    Run the model and returns the output scores
+    Input : Model, eg, RandomForestClassifier, and data: train and test with the labels
+    Output: scores computed from the test data
     '''
-    rf = RandomForestClassifier(n_jobs = -1)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_test)
-    auc_score = roc_auc_score(y_test, y_pred)
-    accuracy = rf.score(X_test, y_test)
-    return accuracy, auc_score
+    m = Model()
+    m.fit(X_train, y_train)
+    y_predict = m.predict(X_test)
+    return accuracy_score(y_test, y_predict), \
+        f1_score(y_test, y_predict), \
+        precision_score(y_test, y_predict), \
+        recall_score(y_test, y_predict),\
+        roc_auc_score(y_test, y_predict)
 
 
-def logistic():
-    lr = LogisticRegression()
-    lr.fit(X_train, y_train)
-    y_pred = lr.predict(X_test)
-    auc_score = roc_auc_score(y_test, y_pred)
-    accuracy = lr.score(X_test, y_test)
-    return accuracy, auc_score
+def compare_models(X, y, models):
+    '''
+    Summarizes the models and print their scores.
+    Input: X_data, y_data and models
+    Output: None
+    '''
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
+    X_train1, X_train2,y_train1, y_train2 = train_test_split(X_train, y_train, test_size = 0.8, random_state = 1234)
 
 
-def decisiontree():
-    dt = DecisionTreeClassifier()
-    dt.fit(X_train, y_train)
-    y_pred = dt.predict(X_test)
-    auc_score = roc_auc_score(y_test, y_pred)
-    accuracy = dt.score(X_test, y_test)
-    return accuracy, auc_score
+    print '='*80
+    print "acc\tf1\tprec\trecall\tauc score"
+    for Model in models:
+        name = Model.__name__
+        acc, f1, prec, rec, auc = run_model(Model, X_train2, X_test, y_train2, y_test)
+        print "%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%s" % (acc, f1, prec, rec, auc, name)
+    print '='*80
 
-def gradboost():
-    gbc = GradientBoostingClassifier(n_estimators=500, max_depth=8, subsample=0.5,
-                                 max_features='auto', learning_rate=0.05)
-    gbc.fit(X_train, y_train)
-    y_pred = gbc.predict(X_test)
-    auc_score = roc_auc_score(y_test, y_pred)
-    accuracy = gbc.score(X_test, y_test)
-    return accuracy, auc_score
-
-
-def knn():
-    kn = KNeighborsClassifier()
-    kn.fit(X_train, y_train)
-    y_pred = kn.predict(X_test)
-    auc_score = roc_auc_score(y_test, y_pred)
-    accuracy = kn.score(X_test, y_test)
-    return accuracy, auc_score
-
-
-def svm():
-    sv = SVC()
-    sv.fit(X_train, y_train)
-    y_pred = sv.predict(X_test)
-    auc_score = roc_auc_score(y_test, y_pred)
-    accuracy = sv.score(X_test, y_test)
-    return accuracy, auc_score
-
-
-def plot_importance(clf, X, max_features=10):
-    '''Plot feature importance'''
-    feature_importance = clf.feature_importances_
-    # make importances relative to max importance
-    feature_importance = 100.0 * (feature_importance / feature_importance.max())
-    sorted_idx = np.argsort(feature_importance)
-    pos = np.arange(sorted_idx.shape[0]) + .5
-
-    # Show only top features
-    pos = pos[-max_features:]
-    feature_importance = (feature_importance[sorted_idx])[-max_features:]
-    feature_names = (X.columns[sorted_idx])[-max_features:]
-
-    plt.barh(pos, feature_importance, align='center')
-    plt.yticks(pos, feature_names)
-    plt.xlabel('Relative Importance')
-    plt.title('Variable Importance')
-
-
-
-def run_model():
-
-
-
-    print 'random forest: ', random_forest()
-    print 'logistic: ', logistic()
-    print 'decision tree: ', decisiontree()
-    print 'grad boost: ', gradboost()
-    print 'knn: ', knn()
-    print 'svm: ', svm()
 
 def baseline_mat():
+    '''
+    returns the X and y data from the dataframe, selecting only few features.
+    Input: None
+    Output: X and y data
+    '''
     df = pd.read_csv('../data/featured_data.csv')
     y = (df.percentage > 0.5).astype(int)
     use_cols = ['days','goal','shares']
@@ -114,14 +60,49 @@ def baseline_mat():
     return X, y
 
 
+def feature_mat():
+    '''
+    returns the feature matrix and labels
+    '''
+    with open('../data/vectorizer.pkl') as f:
+        vectorizer = pickle.load(f)
+    with open('../data/sparse_mat.pkl') as f:
+        sparse_mat = pickle.load(f)
+
+    with open('../data/nmf_model.pkl') as f:
+        nmf_model = pickle.load(f)
+    with open('../data/mnb_model.pkl') as f:
+        mnb_model = pickle.load(f)
+    df = pd.read_csv('../data/featured_data.csv')
+    df2 = pd.get_dummies(df.category)
+    df = pd.concat((df, df2), axis =1)
+
+    nmf_topics = nmf_model.transform(sparse_mat.toarray())
+    df3 = pd.DataFrame(nmf_topics)
+    df = pd.concat((df, df3), axis = 1)
+    df['mnb_probs'] = mnb_model.predict_proba(sparse_mat)[:,1]
+
+    y = (df.percentage > 0.5).astype(int)
+    drop_list = ['category', 'name','place','story','title','date_created','date_recorded','raised','people','percentage',\
+                'average_contribution']
+
+    df.drop(drop_list, axis =1, inplace = True)
+    X = df.values
+
+    return X, y
+
+
 if __name__ == '__main__':
+
+    models = [LogisticRegression, DecisionTreeClassifier, RandomForestClassifier,\
+    GradientBoostingClassifier, AdaBoostClassifier, KNeighborsClassifier, SVC]
+    print '='*80
     print 'baseline scores....'
-    print '================================'
     X, y = baseline_mat()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
-    run_model()
+    compare_models(X, y, models)
+
     print 'scores after featurizing....'
-    print '================================='
+
     X, y = feature_mat()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
-    run_model()
+    compare_models(X, y, models)
